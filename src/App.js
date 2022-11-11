580339b4a5199f2222b72422fcd10e9b443c1aa6
Stepan <stepa.davydov.2002@mail.ru>
2022-11-11 20:11:43 +0300

    Toast to login/register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MantineProvider, Text } from '@mantine/core';
 import { HeaderResponsive } from './components/Header/Header';
 import {IconGolf,IconTrophy,IconShoppingCart,IconUser} from '@tabler/icons';
 import { BrowserRouter as Router, Redirect,Route,Routes  } from 'react-router-dom';
+import { NotificationsProvider } from '@mantine/notifications';
 
 import {Main} from './pages/Main/Main';
 import {Play} from './pages/Play/Play';
@@ -18,53 +19,52 @@ export default function App() {
         auth: !true,
     });
     const [opened, setOpened] = useState(false);
-    // const [page, setPageg] = useState('#play');
-    //arrow function auth
+
     const auth = () => setOpened(true);
     const closeModal = () => setOpened(false);
     return (
         <Router>
             
-            <MantineProvider withGlobalStyles withNormalizeCSS theme={{
-                colorScheme: 'dark'}}>
+            <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme: 'dark'}}>
+                <NotificationsProvider position="top-right" >
                     <LoginModal opened={opened} closeModal={closeModal}/>
-                <HeaderResponsive auth={auth} links={user.auth ? [
-                {
-                    label: 'Играть',
-                    link: '/play',
-                    icon: <IconGolf/>
-                },
-                {
-                    label: 'Топ',
-                    link: '/top',
-                    icon: <IconTrophy/>
-                },
-                {
-                    label: 'Магазин',
-                    link: '/shop',
-                    icon: <IconShoppingCart/>
-                },
-                {
-                    label: 'Профиль',
-                    link: '/profile',
-                    icon: <IconUser/>
-                },
-                ] : []} />
-                
-                <Routes>
+                    <HeaderResponsive auth={auth} links={user.auth ? [
+                    {
+                        label: 'Играть',
+                        link: '/play',
+                        icon: <IconGolf/>
+                    },
+                    {
+                        label: 'Топ',
+                        link: '/top',
+                        icon: <IconTrophy/>
+                    },
+                    {
+                        label: 'Магазин',
+                        link: '/shop',
+                        icon: <IconShoppingCart/>
+                    },
+                    {
+                        label: 'Профиль',
+                        link: '/profile',
+                        icon: <IconUser/>
+                    },
+                    ] : []} />
                     
-                    {user.auth ? <>
-                    <Route index path="/play" element={<Play />} />
-                    <Route path="/top" element={<Top />} />
-                    <Route path="/shop" element={<Shop />} />
-                    <Route path="/profile" element={<Profile />} />
-                    </>:
-                    <Route index path="/" element={<Main />} />
-                    }
+                    <Routes>
+                        
+                        {user.auth ? <>
+                        <Route index path="/play" element={<Play />} />
+                        <Route path="/top" element={<Top />} />
+                        <Route path="/shop" element={<Shop />} />
+                        <Route path="/profile" element={<Profile />} />
+                        </>:
+                        <Route index path="/" element={<Main />} />
+                        }
 
-                    {/* <Route path="*" element={<Main />} /> */}
-                </Routes>
-
+                        {/* <Route path="*" element={<Main />} /> */}
+                    </Routes>
+                </NotificationsProvider>
             </MantineProvider>
         </Router>
         
--- src/components/Main/LoginModal.js
+++ src/components/Main/LoginModal.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
-import { Modal, Button, Space,TextInput,PasswordInput,Text  } from '@mantine/core';
+import { useState, useRef  } from 'react';
+import { Modal, Button, Space,TextInput,PasswordInput,Text   } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
 import { useMediaQuery } from '@mantine/hooks';
-import { IconAt,IconLock,IconUser } from '@tabler/icons';
+import { IconAt,IconLock,IconUser,IconAlertTriangle } from '@tabler/icons';
 
 
 export function LoginModal({opened, closeModal}) {
     const [modalType, setModalType] = useState("login");
     const isMobile = useMediaQuery('(max-width: 600px)');
+
+    const   email = useRef(null),
+            password = useRef(null),
+            login = useRef(null),
+            password2 = useRef(null);
+
+    const auth = () =>{
+        console.log(email.current.value,password.current.value);
+    }
+    const register = () =>{
+        console.log(login.current.value,email.current.value,password.current.value,password2.current.value);
+        if (password.current.value !== password2.current.value) {
+            showNotification({
+                message: 'Пароли не совпадают',
+                color: 'red',
+                icon: <IconAlertTriangle size={18}/>,
+            })
+        }
+    }
     return (
         <>
         <Modal
+            radius={8}
             opened={opened}
             onClose={() => closeModal()}
             title={modalType=="login" ? <Text size={20} weight="bold">Авторизация</Text> : <Text size={20} weight="bold">Регистрация</Text>}
@@ -17,37 +38,43 @@ export function LoginModal({opened, closeModal}) {
             fullScreen={isMobile} 
         >
             {modalType=="login" ? <>
-                <TextInput label="Email" placeholder="Email" icon={<IconAt size={14} />} />
+                <TextInput label="Email" placeholder="Email" icon={<IconAt size={14} />} radius={8} ref={email}/>
                 <Space h="xs" />
                 <PasswordInput
                     placeholder="Пароль"
                     label="Пароль"
                     icon={<IconLock size={16} />}
+                    radius={8}
+                    ref={password}
                 />
                 <Space h="md" />
-                <Button fullWidth variant="fill" onClick={() => 1}>
+                <Button fullWidth variant="fill" onClick={() => auth()}>
                     Войти
                 </Button>
                 <Space h="xs" />
                 <Text onClick={()=>setModalType("register")} size={14} align="center" color="dimmed" variant="link" style={{cursor: "pointer", userSelect:"none"}}>Нет аккаунта? Зарегестрируйтесь</Text>
             </> : <>
-                <TextInput label="Логин" placeholder="Придумайте логин" icon={<IconUser size={14} />} />
+                <TextInput label="Логин" placeholder="Придумайте логин" icon={<IconUser size={14} />} radius={8} ref={login}/>
                 <Space h="xs" />
-                <TextInput label="Email" placeholder="Email" type="email" icon={<IconAt size={14} />} />
+                <TextInput label="Email" placeholder="Email" type="email" icon={<IconAt size={14} />} radius={8} ref={email}/>
                 <Space h="xs" />
                 <PasswordInput
                     placeholder="Пароль"
                     label="Пароль"
                     icon={<IconLock size={16} />}
+                    radius={8}
+                    ref={password}
                 />
                 <Space h="xs" />
                 <PasswordInput
                     placeholder="Повторите пароль"
                     label="Повторите пароль"
                     icon={<IconLock size={16} />}
+                    radius={8}
+                    ref={password2}
                 />
                 <Space h="md" />
-                <Button fullWidth variant="fill" onClick={() => 1}>
+                <Button fullWidth variant="fill" onClick={() => register()}>
                     Зарегестрироваться
                 </Button>
                 <Space h="xs" />
